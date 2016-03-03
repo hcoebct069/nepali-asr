a68a74cf1668ff645816c27370488fa4a682f4a6
Nootan Ghimire <nootan.ghimire@gmail.com>
2016-03-03 09:23:58 +0545

    Basic Cleanup

--- codes/correctedDemo.py
+++ codes/correctedDemo.py
@@ -1,12 +1,18 @@
-import soundlab as sf
+import soundfile as sf
 import numpy
-import matplotlib.plot as plt
+import matplotlib.pyplot as plt
 
 stereo_audio_data, sample_rate = sf.read('../recordings/0.ogg')
 mono_audio_data = stereo_audio_data[:,1] #uses 2nd channel :) 
 
 plt.plot(mono_audio_data)
 plt.savefig('mono_audio_data_zero.png')
+plt.clf()
+
+data = numpy.asfarray(list(filter(lambda x: x > 0.1 or x < -0.1, mono_audio_data)))
+
+plt.plot(data)
+plt.savefig('processed_audio_data_zero.png')
 plt.clf()
 
 #write code to calculate envelope
